d5f9e76aa5747743c8af430c92e01c1a237c7ac9
Upamanyu Sharma <upamanyu@mit.edu>
2021-09-15 00:07:10 -0400

    Functioning remote peak

--- remote_peaks.py
+++ remote_peaks.py
@@ -53,7 +53,10 @@ def run_command(args, cwd=None):
         return subprocess.run(args, capture_output=True, text=True, cwd=cwd)
 
 def run_remote_command(host:str, cmd:str, cwd=None):
-    run_command(["ssh", host, '"' + cmd + '"'])
+    run_command(["ssh", host, cmd])
+
+def start_remote_command(host:str, cmd:str, cwd=None):
+    start_command(["ssh", host, cmd])
 
 def start_command(args, cwd=None):
     if global_args.dry_run or global_args.verbose:
@@ -91,11 +94,12 @@ def start_memkv_coord(initsrv):
 
 def install_shard_remote(host:str):
     # XXX: make sure it's as up-to-date as possible
-    run_remote_command(host, "go install github.com/mit-pdos/gokv/cmd/memkvshard@latest")
+    run_remote_command(host, "go install github.com/mit-pdos/gokv/cmd/memkvshard")
 
 def start_remote_shard_server(host:str, port:int, corelist:list[int], init:bool):
     c = ",".join([str(j) for j in corelist])
-    run_remote_command(host, "nohup numactl" + c + "~/go/bin/memkvshard -port " + str(port) + (init * " -init"))
+    start_remote_command(host, "sleep 10 && echo test")
+    start_remote_command(host, "numactl -C " + c + " ~/go/bin/memkvshard -port " + str(port) + (init * " -init"))
     # XXX: add check to see if it's running
     print("[INFO] Started a remote shard server with {0} cores at {1}:{2}".format(len(corelist), host, port))
 
@@ -188,12 +192,11 @@ def main():
     os.makedirs(global_args.outdir, exist_ok=True)
     resource.setrlimit(resource.RLIMIT_NOFILE, (100000, 100000))
 
-    rh = 'pd7.csail.mit.edu'
     r = '18.26.5.7' # pd7
-    install_shard_remote(rh)
+    install_shard_remote(r)
     start_memkv_coord(r + ':12300')
-    stop_remote_shard_server(rh)
-    start_remote_shard_server(rh, 12300, range(1), True)
+    stop_remote_shard_server(r)
+    start_remote_shard_server(r, 12300, range(1), True)
 
     threads, peak = find_peak_thruput(128, 'memkv_peak_raw.jsons', 0.95, 0.05, range(1,4))
     with open(path.join(global_args.outdir, 'memkv_peaks.jsons'), 'a+') as outfile:
